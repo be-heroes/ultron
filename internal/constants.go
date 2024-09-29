2b53bd5ee99db3789dca25e40b00d4cc1afbbb48
Tobias Andersen <tobias.andersen79@gmail.com>
2024-09-29 14:48:05 +0200

    move constants to seperate file

---
+++ internal/constants.go
@@ -0,0 +1,24 @@
+package internal
+
+const (
+	AnnotationDiskType    = "ultron.io/disk-type"
+	AnnotationNetworkType = "ultron.io/network-type"
+	AnnotationStorageSize = "ultron.io/storage-size"
+	AnnotationPriority    = "ultron.io/priority"
+
+	CacheKeyWeightedNodes           = "WEIGHTED_NODES"
+	CacheKeyDurableVmConfigurations = "DURABLE_VMCONFIGURATION"
+	CacheKeySpotVmConfigurations    = "SPOT_VMCONFIGURATION"
+
+	DefaultDiskType              = "SSD"
+	DefaultNetworkType           = "isolated"
+	DefaultStorageSizeGB         = 10.0
+	DefaultPriority              = PriorityLow
+	DefaultDurableInstanceType   = "ultron.durable"
+	DefaultEphemeralInstanceType = "ultron.ephemeral"
+
+	LabelHostName     = "kubernetes.io/hostname"
+	LabelInstanceType = "node.kubernetes.io/instance-type"
+
+	MetadataName = "metadata.name"
+)
